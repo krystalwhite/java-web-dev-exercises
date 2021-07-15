bbab0babf6dca326d4d85377bd3e68c2ad8f2a93
krystalwhite <krystalsnowhite@gmail.com>
2021-07-14 20:46:49 -0500

    bonus attempts

--- src/org/launchcode/java/studios/areaofcircle/Area.java
+++ src/org/launchcode/java/studios/areaofcircle/Area.java
@@ -8,11 +8,42 @@ public class Area {
 
         System.out.println("What is the radius of the circle? ");
         double radius = input.nextDouble();
+        while (radius <=0) {
+            System.out.println("Please enter a positive number for the radius.");
+            radius = input.nextDouble();
+        }
+        input.close();
 
 //        double area = radius * radius * Math.PI;
         double area = Circle.getArea(radius);
         System.out.println("Your circle with a radius of " + radius + " units has an approximate area of " + area + " units squared.");
 
-        input.close();
+//        calling the calculateCircleArea method
+        System.out.println("Your circle with a radius of " + radius + " units has an approximate area of " + calculateCircleArea(radius) + " units squared.");
+
+
+//        different code attempt with studio partners
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("What is the radius of the circle? ");
+        String userRadius = userInput.nextLine();
+        if (userRadius.equals("")) {
+            System.out.println("You did not enter in a quantity.");
+        }
+
+        Double userRadiusNumber = Double.parseDouble(userRadius);
+
+        System.out.println("Your circle with a radius of " + userRadiusNumber + " units has an approximate area of " + calculateCircleArea(userRadiusNumber) + " units squared.");
+        userInput.close();
+
+
     }
+
+
+    //        alternate path to write a new method
+        public static double calculateCircleArea(double userRadius) {
+            return Math.PI * userRadius * userRadius;
+        }
+
+
 }
