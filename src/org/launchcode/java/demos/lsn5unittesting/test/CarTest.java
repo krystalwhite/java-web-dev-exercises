c8ed5fd9b1d689f06ce8bbcc919721d0cef3bc49
krystalwhite <krystalsnowhite@gmail.com>
2021-07-28 14:06:03 -0500

    created unit tests

--- src/org/launchcode/java/demos/lsn5unittesting/test/CarTest.java
+++ src/org/launchcode/java/demos/lsn5unittesting/test/CarTest.java
@@ -1,5 +1,13 @@
 package org.launchcode.java.demos.lsn5unittesting.test;
 
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.launchcode.java.demos.lsn5unittesting.main.Car;
+
+
 public class CarTest {
 
     //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitLab account)
@@ -7,5 +15,31 @@ public class CarTest {
     //TODO: gasTankLevel is accurate after driving within tank range
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
     //TODO: can't have more gas than tank size, expect an exception
+
+    Car test_car;
+    @Before
+    public void createCarObject() {
+        test_car = new Car("Toyota", "Prius", 10, 50);
+    }
+
+    @Test
+    public void emptyTest() {
+        assertEquals(10,10,.001);
+    }
+
+    @Test
+    public void testInitialGasTank() {
+        assertEquals(10, test_car.getGasTankLevel(), 0.001);
+    }
+
+    @Test
+    public void testInitialGasTank2() {
+        assertFalse(test_car.getGasTankLevel() == 0);
+    }
+
+    @Test
+    public void testInitialGasTank3() {
+        assertTrue(test_car.getGasTankLevel() == 10);
+    }
 
 }
