d6d449a54e10c24bc3e55e5609d7aa13c00b8365
krystalwhite <krystalsnowhite@gmail.com>
2021-07-28 20:39:10 -0500

    starting frame for menu and menuitem

---
+++ src/org/launchcode/java/studio/restaurantmenu/src/restaurant/Menu.java
@@ -0,0 +1,12 @@
+package restaurant;
+
+public class Menu {
+//    public void main (String[] args) {
+
+        MenuItem tiramisu = new MenuItem("dessert", "Italian gooey goodness", 6.99, false);
+
+//        methods to create:
+//        new item: if item is x amount of days old then go to false
+//        pulling key and using hashmap (ex: pull all apps and print those menu items)
+//    }
+}
---
+++ src/org/launchcode/java/studio/restaurantmenu/src/restaurant/MenuItem.java
@@ -0,0 +1,49 @@
+package restaurant;
+
+public class MenuItem {
+
+    private String category;
+    private String description;
+    private Double price;
+    private Boolean newItem = false;
+
+    public MenuItem (String category, String description, Double price, Boolean newItem) {
+        this.category = category;
+        this.description = description;
+        this.price = price;
+        this.newItem = newItem;
+    }
+
+//    getters and setters for Menu Items
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Boolean getNewItem() {
+        return newItem;
+    }
+
+    public void setNewItem(Boolean newItem) {
+        this.newItem = newItem;
+    }
+}
