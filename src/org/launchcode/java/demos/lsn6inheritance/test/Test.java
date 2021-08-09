1c8ec5f513252e03fdd60875645e74e632408e48
krystalwhite <krystalsnowhite@gmail.com>
2021-08-09 18:04:21 -0500

    practice demo

---
+++ src/org/launchcode/java/demos/lsn6inheritance/test/Test.java
@@ -0,0 +1,13 @@
+package org.launchcode.java.demos.lsn6inheritance.test;
+import org.launchcode.java.demos.lsn6inheritance.Cat;
+import org.launchcode.java.demos.lsn6inheritance.HouseCat;
+import static org.junit.Assert.assertEquals;
+
+public class Test {
+
+    @org.junit.Test
+        public void inheritsSuperInFirstConstructor() {
+            HouseCat keyboardCat = new HouseCat("Keyboard Cat", 7);
+            assertEquals(7, keyboardCat.getWeight(), .001);
+        }
+}
