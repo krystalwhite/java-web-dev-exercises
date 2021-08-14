7748d52b2730e916365d49b05e394ddcd15aaf4a
krystalwhite <krystalsnowhite@gmail.com>
2021-08-14 15:12:32 -0500

    verified createID method works

--- src/exercises/technology/Main.java
+++ src/exercises/technology/Main.java
@@ -7,8 +7,9 @@ public class Main {
         Laptop laptop = new Laptop("Lenovo Thinkpad X1 Titanium Yoga", 16, 1769.40, 13.5, true, 2.54, true, true);
         SmartPhone smartPhone = new SmartPhone("Moto G Play", 3, 169.99, 6.5, false, 1, true);
 
-        System.out.println("Computer ID: " + computer.getIdCounter());
-        System.out.println("Laptop ID: " + laptop.getIdCounter());
-        System.out.println("SmartPhone ID: " + smartPhone.getIdCounter());
+
+        System.out.println("Computer ID: " + computer.createID());
+        System.out.println("Laptop ID: " + laptop.createID());
+        System.out.println("SmartPhone ID: " + smartPhone.createID());
     }
 }
